2b12c06dd0a8bb07c59f60b1d270132844ea7569
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-12 22:23:13 +1200

    Creating the database

--- Social_Activity_Planner/API/Program.cs
+++ Social_Activity_Planner/API/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +15,24 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host= CreateWebHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch(Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            host.Run(); // start the app
         }
 
         public static IHostBuilder CreateWebHostBuilder(string[] args) =>
--- Social_Activity_Planner/Persistence/DataContext.cs
+++ Social_Activity_Planner/Persistence/DataContext.cs
@@ -15,6 +15,6 @@ namespace Persistence
             }
         }
 
-        public DbSet<Activity> MyProperty { get; set; }
+        public DbSet<Activity> Activities { get; set; } // database table
     }
 }
