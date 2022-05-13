4f97c0a989667b0409baf8ae652153457b068c79
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-13 20:41:58 +1200

    Fetching data from the API
    there is some error in CORS Policy

--- Social_Activity_Planner/client-app/src/App.tsx
+++ Social_Activity_Planner/client-app/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    const [activities, setActivities] = useState([]); // set activity initial state
+
+    // fetch activities from API server 
+    useEffect(() => { // function with no parameter 
+        axios.get('http://localhost:5000/api/activities').then(response => {
+            console.log(response); // see axios in console
+            setActivities(response.data); // set activity to the response we get from axios
+        })
+    }, []) // use empty array to ensure function only runs 1 times, not endless loop
   return (
     <div className="App">
       <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+              <ul>
+                  {activities.map((activity: any)=> ( // need to add type of activity
+                      <li key={activity.id}>
+                          {activity.title}
+                      </li>
+                   ))}
+              </ul>
       </header>
     </div>
   );
