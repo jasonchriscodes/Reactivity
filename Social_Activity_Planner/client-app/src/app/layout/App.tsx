828eb6c2d0cc2f19934fecc3ab70fc24cd19c3f5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-14 20:55:43 +1200

    Adding some style to the Navbar

--- Social_Activity_Planner/client-app/src/app/layout/App.tsx
+++ Social_Activity_Planner/client-app/src/app/layout/App.tsx
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
-import NavBar from './NavBar';
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
-    const [activities, setActivities] = useState<Activity[]>([]); // set activity initial state
+  const [activities, setActivities] = useState<Activity[]>([]); // set activity initial state
 
-    // fetch activities from API server 
-    useEffect(() => { // function with no parameter 
-        axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-            setActivities(response.data); // set activity to the response we get from axios, get type safety from activity.ts
-        })
-    }, []) // use empty array to ensure function only runs 1 times, not endless loop
+  // fetch activities from API server
+  useEffect(() => {
+    // function with no parameter
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data); // set activity to the response we get from axios, get type safety from activity.ts
+      });
+  }, []); // use empty array to ensure function only runs 1 times, not endless loop
+  // <> and </> is Fragment shortcut
   return (
-      <div>
-          <NavBar/>
-          <List>
-              {activities.map(activity => ( // need to add type of activity
-                  <List.Item key={activity.id}>
-                      {activity.title} 
-                  </List.Item>
-              ))}
-          </List>
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map(
+            (
+              activity // need to add type of activity
+            ) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            )
+          )}
+        </List>
+      </Container>
+    </>
   );
 }
 
