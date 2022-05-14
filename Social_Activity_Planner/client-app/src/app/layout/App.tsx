fa873bb70719715f0a8ba83b1e1971e4c86a6fb8
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-14 15:35:59 +1200

    Adding an Activity interface

--- Social_Activity_Planner/client-app/src/app/layout/App.tsx
+++ Social_Activity_Planner/client-app/src/app/layout/App.tsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-    const [activities, setActivities] = useState([]); // set activity initial state
+    const [activities, setActivities] = useState<Activity[]>([]); // set activity initial state
 
     // fetch activities from API server 
     useEffect(() => { // function with no parameter 
-        axios.get('http://localhost:5000/api/activities').then(response => {
-            setActivities(response.data); // set activity to the response we get from axios
+        axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+            setActivities(response.data); // set activity to the response we get from axios, get type safety from activity.ts
         })
     }, []) // use empty array to ensure function only runs 1 times, not endless loop
   return (
       <div>
           <Header as='h2' icon='users' content='ActivityBud' />
           <List>
-              {activities.map((activity: any) => ( // need to add type of activity
+              {activities.map(activity => ( // need to add type of activity
                   <List.Item key={activity.id}>
-                      {activity.title}
+                      {activity.title} // get type safety because we import activity.ts
                   </List.Item>
               ))}
           </List>
