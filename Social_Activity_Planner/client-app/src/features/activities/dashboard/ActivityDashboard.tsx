29a7d198329dad2ec8d46919b99a5f4cf1befcfb
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-14 22:01:43 +1200

    Creating an activity form

--- Social_Activity_Planner/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Social_Activity_Planner/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, GridColumn, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 interface Props {
@@ -17,6 +18,7 @@ export default function ActivityDashboard({ activities }: Props) {
       <Grid.Column width="6">
         {/* && means anything to the right will execute as long as activities[0] is NOT null or undefine */}
         {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
