dc7a28ba2fd2f8d83c6b66bc437a60485b028573
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-15 17:43:13 +1200

    Getting an individual activity

--- Social_Activity_Planner/client-app/src/app/stores/activityStore.ts
+++ Social_Activity_Planner/client-app/src/app/stores/activityStore.ts
@@ -2,7 +2,6 @@ import { Activity } from "./../models/activity";
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { v4 as uuid } from "uuid";
-import { string } from "yup";
 
 export default class ActivityStore {
   activityRegistry = new Map<string, Activity>();
@@ -26,8 +25,7 @@ export default class ActivityStore {
       const activities = await agent.Activities.list();
 
       activities.forEach((activity) => {
-        activity.date = activity.date.split("T")[0]; // only date not include time info
-        this.activityRegistry.set(activity.id, activity);
+        this.setActivity(activity);
       });
       this.setLoadingInitial(false);
     } catch (error) {
@@ -36,27 +34,34 @@ export default class ActivityStore {
     }
   };
 
+  loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if (activity) {
+      this.selectedActivity = activity;
+    } else {
+      this.loadingInitial = true;
+      try {
+        activity = await agent.Activities.details(id);
+        this.setActivity(activity);
+        this.setLoadingInitial(false);
+      } catch (error) {
+        console.log(error);
+        this.setLoadingInitial(false);
+      }
+    }
+  };
+
+  private setActivity = (activity: Activity) => {
+    activity.date = activity.date.split("T")[0]; // only date not include time info
+    this.activityRegistry.set(activity.id, activity);
+  };
+
+  private getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+  };
+
   setLoadingInitial = (state: boolean) => {
     this.loadingInitial = state;
-  };
-
-  selectActivity = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id); // finding matching object that matches id
-  };
-
-  cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
-  };
-
-  openForm = (id?: string) => {
-    // id? means id is optional
-    // check if id is empty or not, if empty handleSelectActivity will be called, if not handleEditActivity will be called
-    id ? this.selectActivity(id) : this.cancelSelectedActivity();
-    this.editMode = true;
-  };
-
-  closeForm = () => {
-    this.editMode = false;
   };
 
   createActivity = async (activity: Activity) => {
@@ -102,10 +107,6 @@ export default class ActivityStore {
       await agent.Activities.delete(id);
       runInAction(() => {
         this.activityRegistry.delete(id);
-        // delete form if activity deleted in dashboard
-        if (this.selectedActivity?.id === id) {
-          this.cancelSelectedActivity();
-        }
         this.loading = false;
       });
     } catch (error) {
