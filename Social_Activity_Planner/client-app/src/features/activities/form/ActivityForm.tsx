a8a20f322a9aa70622d3abd98d0ca6183e529814
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-15 18:49:17 +1200

    Adding the edit activity route

--- Social_Activity_Planner/client-app/src/features/activities/form/ActivityForm.tsx
+++ Social_Activity_Planner/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,21 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
-  const { selectedActivity, createActivity, updateActivity, loading } =
-    activityStore;
-
-  const initialState = selectedActivity ?? {
+  const {
+    createActivity,
+    updateActivity,
+    loading,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
+  const { id } = useParams<{ id: string }>();
+  const [activity, setActivity] = useState({
     id: "",
     title: "",
     category: "",
@@ -16,9 +23,11 @@ export default observer(function ActivityForm() {
     date: "",
     city: "",
     venue: "",
-  };
+  });
 
-  const [activity, setActivity] = React.useState(initialState);
+  useEffect(() => {
+    if (id) loadActivity(id).then((activity) => setActivity(activity!)); // ! means that the activity will not be undefine
+  }, [id, loadActivity]); // we need to add the id to the dependency array because we want to load the activity only when the id is available, not when the component is rendered (infinity loop)
 
   function handleSubmit() {
     activity.id ? updateActivity(activity) : createActivity(activity);
@@ -31,6 +40,8 @@ export default observer(function ActivityForm() {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value }); // spread operator, assign key to value
   }
+
+  if (loadingInitial) return <LoadingComponent content="Loading activity" />;
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete="off">
