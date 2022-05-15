e769770ca0656d3a860a5ed71b559f602cdbfd01
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-15 19:25:49 +1200

    Adding a key to the route

--- Social_Activity_Planner/client-app/src/app/layout/App.tsx
+++ Social_Activity_Planner/client-app/src/app/layout/App.tsx
@@ -1,13 +1,14 @@
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <NavBar />
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/activities" component={ActivityDashboard} />
         <Route path="/activities/:id" component={ActivityDetails} />
         <Route
+          key={location.key}
           path={["/createActivity", "/manage/:id"]}
           component={ActivityForm}
         />
