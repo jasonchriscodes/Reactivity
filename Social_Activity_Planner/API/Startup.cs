6a3a80a889bc56f6ff20292b3a1949b04b6933c5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-16 15:56:59 +1200

    Configuring identity in the Start up class

--- Social_Activity_Planner/API/Startup.cs
+++ Social_Activity_Planner/API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
     config.RegisterValidatorsFromAssemblyContaining<Create>();
    });
    services.AddApplicationServices(this.config);
+   services.AddIdentityServices(this.config);
   }
 
   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
