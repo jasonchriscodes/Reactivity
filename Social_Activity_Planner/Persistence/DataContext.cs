0ba4b2e783934ab820f9820a265ba6489f47efdd
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-16 15:48:15 +1200

    Refactoring DbContext to add an identity

--- Social_Activity_Planner/Persistence/DataContext.cs
+++ Social_Activity_Planner/Persistence/DataContext.cs
@@ -1,20 +1,21 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
-    {
-        public DataContext() { }
-        public DataContext(DbContextOptions options) : base(options){}
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            if (!options.IsConfigured)
-            {
-                options.UseSqlite("A FALLBACK CONNECTION STRING");
-            }
-        }
+ public class DataContext : IdentityDbContext<AppUser>
+ {
+  public DataContext() { }
+  public DataContext(DbContextOptions options) : base(options) { }
+  protected override void OnConfiguring(DbContextOptionsBuilder options)
+  {
+   if (!options.IsConfigured)
+   {
+    options.UseSqlite("A FALLBACK CONNECTION STRING");
+   }
+  }
 
-        public DbSet<Activity> Activities { get; set; } // database table
-    }
+  public DbSet<Activity> Activities { get; set; } // database table
+ }
 }
