e1eb9a77e95c157ea9da402fcc5183a466d40120
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-16 13:09:51 +1200

    Handling validation error from an invalid GUID

--- Social_Activity_Planner/client-app/src/app/api/agent.ts
+++ Social_Activity_Planner/client-app/src/app/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { config } from "process";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
@@ -19,9 +20,15 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response!;
     switch (status) {
       case 400:
+        if (typeof data === "string") {
+          toast.error(data);
+        }
+        if (config.method == "get" && data.errors.hasOwnProperty("id")) {
+          history.push("/not-found");
+        }
         if (data.errors) {
           const modalStateErrors = [];
           for (const key in data.errors) {
@@ -30,8 +37,6 @@ axios.interceptors.response.use(
             }
           }
           throw modalStateErrors.flat();
-        } else {
-          toast.error(data);
         }
         break;
       case 401:
