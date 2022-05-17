ca00b1f354c8431a4008c526b05a454e71e0e451
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-17 23:37:01 +1200

    Adding a photo controller

---
+++ Social_Activity_Planner/API/Controllers/PhotosController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Application.Photos;
+
+namespace API.Controllers
+{
+ public class PhotosController : BaseApiController
+ {
+  [HttpPost]
+  public async Task<IActionResult> Add([FromForm] Add.Command command)
+  {
+   return HandleResult(await Mediator.Send(command));
+  }
+ }
+}
