faa18741b16e7b0ff674f54d5181aaa292985693
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-17 22:29:29 +1200

    Adding the add photo logic

--- Social_Activity_Planner/Infrastructure/Photos/CloudinarySettings.cs
+++ Social_Activity_Planner/Infrastructure/Photos/CloudinarySettings.cs
@@ -3,7 +3,7 @@ namespace Infrastructure.Photos
  public class CloudinarySettings
  {
   public string CloudName { get; set; }
-  public string Apikey { get; set; }
+  public string ApiKey { get; set; }
   public string ApiSecret { get; set; }
  }
 }
