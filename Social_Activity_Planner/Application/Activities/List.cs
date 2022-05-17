cc8fc7bffa1a742d714e8e305ec8e68b214cc495
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-17 13:35:05 +1200

    Loading related data
    Adding attendees in attendees array when activity added created by user host.
    New Problem: there a possible of infinite loop of object cycle

--- Social_Activity_Planner/Application/Activities/List.cs
+++ Social_Activity_Planner/Application/Activities/List.cs
@@ -29,7 +29,12 @@ namespace Application.Activities
    /// <returns>List of activities</returns>
    public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
    {
-    return Result<List<Activity>>.Success(await this.context.Activities.ToListAsync(cancellationToken));
+    var activities = await this.context.Activities
+    .Include(a => a.Attendees)
+    .ThenInclude(u => u.AppUser)
+    .ToListAsync(cancellationToken);
+
+    return Result<List<Activity>>.Success(activities);
    }
   }
  }
