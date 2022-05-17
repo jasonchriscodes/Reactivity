0536a6d8ecb84649d17a398e84cc06faba692fc2
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-17 17:08:28 +1200

    Adding the attendees component

--- Social_Activity_Planner/client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ Social_Activity_Planner/client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
   activity: Activity;
@@ -27,7 +28,9 @@ export default function ActivityListItem({ activity }: Props) {
         <Icon name="clock" /> {format(activity.date!, "dd MMM yyyy h:mm aa")}
         <Icon name="marker" /> {activity.venue}
       </Segment>
-      <Segment secondary>Attendees go here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendee attendees={activity.attendees!} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
